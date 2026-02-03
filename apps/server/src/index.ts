cc6c15c3c061b8acf497e60d3436ee947ed7af2a
Itz-Agasta <rupamgolui69@gmail.com>
2026-02-03 17:11:52 +0530

    Add Pino logger middleware and error handling

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,11 +1,11 @@
 import { env } from "@LogPose/env/server";
 import { Hono } from "hono";
 import { cors } from "hono/cors";
-import { logger } from "hono/logger";
 
-const app = new Hono();
+import pinoLogger from "./middlewares/logger";
 
-app.use(logger());
+const app = new Hono().use(pinoLogger);
+
 app.use(
   "/*",
   cors({
@@ -15,7 +15,35 @@ app.use(
 );
 
 app.get("/", (c) => {
-  return c.text("OK");
+  return c.json("OK form Vyse");
+});
+
+app.get("/error", (c) => {
+  c.status(422);
+  c.var.logger.warn("error");
+  throw new Error("oh no");
+});
+
+app.notFound((c) => {
+  return c.json(
+    {
+      success: false,
+      message: "Not Found - " + c.req.url,
+    },
+    404,
+  );
+});
+
+app.onError((err, c) => {
+  return c.json(
+    {
+      success: false,
+      message: err.message,
+      error: err,
+      stack: process.env.NODE_ENV === "production" ? null : err.stack,
+    },
+    500,
+  );
 });
 
 export default app;
---
+++ apps/server/src/middlewares/logger.ts
@@ -0,0 +1,26 @@
+import pino from "pino";
+import { pinoLogger } from "hono-pino";
+import type { DebugLogOptions } from "hono-pino/debug-log";
+
+const options: DebugLogOptions = {
+  colorEnabled: true,
+};
+
+const isProduction = process.env.NODE_ENV === "production";
+
+export default pinoLogger({
+  pino: pino({
+    base: null,
+    ...(isProduction
+      ? { level: "trace" }
+      : {
+          level: "info",
+          transport: {
+            target: "hono-pino/debug-log",
+            options,
+          },
+        }),
+    timestamp: pino.stdTimeFunctions.unixTime,
+  }),
+  http: { reqId: () => crypto.randomUUID() },
+});
